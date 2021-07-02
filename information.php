298ecdda1cf0060510d2f6eef2f2baca40b08161
israkkayum <israk.kayum@gmail.com>
2021-07-03 01:30:57 +0600

    first commit

---
+++ information.php
@@ -0,0 +1,22 @@
+<?php
+
+    $Name = $_POST['Name'];
+	$Email = $_POST['Email'];
+	$Subject = $_POST['Subject'];
+	$Message = $_POST['Message'];
+	
+	$conn = new mysqli('sql212.epizy.com', 'epiz_26594656','6C0Ay8Z78cFE2','epiz_26594656_message');
+	if($conn->connect_error){
+		die('Connection Failed :' .$conn->connect_error);
+	} else{
+		$stmt = $conn->prepare("insert into information(Name, Email, Subject, Message)
+		values(?, ?, ?, ?)");
+		$stmt->bind_param("ssss",$Name, $Email, $Subject, $Message);
+		$stmt->execute();
+		echo "Submit Successful";
+		$stmt->close();
+		$conn->close();
+		
+	}
+
+?>
